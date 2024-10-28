988db97accce1a9e623285d82de6dc5a7d2d0737
Antoine Rey <antoine.rey@free.fr>
2024-10-28 18:56:00 +0100

    Add a QueryRouter to utilize the Embedding Store when appropriate

--- src/main/java/org/springframework/samples/petclinic/chat/AssistantConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/chat/AssistantConfiguration.java
@@ -3,8 +3,12 @@ package org.springframework.samples.petclinic.chat;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import org.springframework.context.annotation.Bean;
@@ -35,6 +39,13 @@ class AssistantConfiguration {
 	EmbeddingStoreContentRetriever contentRetriever(InMemoryEmbeddingStore<TextSegment> embeddingStore,
 			EmbeddingModel embeddingModel) {
 		return new EmbeddingStoreContentRetriever(embeddingStore, embeddingModel);
+	}
+
+	@Bean
+	RetrievalAugmentor retrievalAugmentor(ChatLanguageModel chatLanguageModel, ContentRetriever vetContentRetriever) {
+		return DefaultRetrievalAugmentor.builder()
+			.queryRouter(new VetQueryRouter(chatLanguageModel, vetContentRetriever))
+			.build();
 	}
 
 }
